343daee9a1952e71adda31816dc29e7885d2a544
Aestas <hawhawba@gmail.com>
2021-07-12 20:00:33 +0900

    card 구성 및 디자인

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,6 +1,33 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>Home Page</h5>
+  <q-page
+    class="constrain q-pa-md"
+  >
+    <q-card class="card-post" flat bordered>
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://blogpfthumb-phinf.pstatic.net/MjAyMDEwMDVfMTIx/MDAxNjAxOTAyNDgyNTMx.BSriRdPZ1q6zf_4_AnoyxTlNqeP3j8LPdDatjWjZAqcg.U9aLxWjubNO3KaV40_CQL5JBRHkAQskruUIYma9mpQgg.JPEG.iissy85/MU.jpeg?type=w161">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>aestas_kwak</q-item-label>
+          <q-item-label caption>
+            Seoul, Korea
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator />
+      
+      <q-img src="https://lh3.googleusercontent.com/fife/AAWUweWbkcsPtaxSF7VCJRHzGYv3p0dX2_d0RlIQ9JlNB9FC-oJrRKJPnUMDJtFy2KYnfwsS0zMR_LHRKJE6PNxpfowDpGbEW9bD89akFLjvLs2U_pJY23qzKE2wHxSMFvmlNezbTNOa3v_Et9CS4DPadpvRR6NtG6V8DHUHO_Gn6nbHIByoMEwmyD4FmQ8EyL1w31P6Okh7TPPuP0KtpB0FCW2sEA-9f0HHdwZ94L0AgXtktAZyQYKLPHFk2SxXbEc8vfXuKw2F5JPYyzXmF5wEK0i1o_1bi7Pi9pziarblz4VRVl8S6TsK_46ivSWIl2SHdwO81XWSvkPwPjn3bDwN5K9uZJUuPxW2U4C4efLwCb85kY00P43Q-A9K9Op6UuHqw0dtxNxGvhUZ1Zpwe3jbbtuT5Eyb8Ber_BHJGYphyfLhAX22CRH-nzEAJRSyRdaUrXMEn3ec7BSlNrK9WofFFiOUh4atZKjg1I0Ogid8aXbMv7txH5sCC6BTOmLw_igWQSXifdAip2FDWk8iWXdQOjyziQvtJzbcOuKJdWMObI3kOjc9eyn-59N7z-zv7TqJSM2Kx9r2IKIVZgyS3PNMgFVau0TdS0ybKd6IB8B06ybZHGvGWDYHt7DBBc7gqi5bYAMjzTUfhRS9f538IAgfmLs6UEVV-4Q_VXXlS4Fr8c1Ze3Os0FSUcwTfFo0_8hYR05oBq-JyTHdYYhl6z81QzUENGdoxzIR2jA=w2346-h1788-ft" />
+
+      <q-card-section>
+        <div>A dog sitting with his back to a person</div>
+        <div class="text-caption text-grey">July 12 7:56PM</div>
+      </q-card-section>
+
+    </q-card>
   </q-page>
 </template>
 
@@ -9,3 +36,9 @@ export default {
   name: 'PageHome'
 }
 </script>
+
+<style lang="sass">
+  .card-post
+    .q-img
+      min-height: 200px
+</style>
