40ff9a5b87efd6331cf870239be0ddbb3d4fffb1
BongSeon <hawhawba@gmail.com>
2021-07-18 18:20:26 +0900

    백엔드 - firebase 컨넥팅 테스트 성공

--- backend/index.js
+++ backend/index.js
@@ -28,17 +28,14 @@
   endpoint - posts
 */
   app.get('/posts', (request, response) => {
-    let posts = [
-      {
-        caption: 'Golden Gate Bridge',
-        location: 'San Francisco'
-      },
-      {
-        caption: 'London Eye',
-        location: 'Londom'
-      }
-    ]
-    response.send(posts)
+    let posts = []
+
+    db.collection('posts').get().then(snapshot => {
+      snapshot.forEach((doc) => {
+        posts.push(doc.data())
+      });
+      response.send(posts)
+    })
   })
 
 /*
