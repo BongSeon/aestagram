4686b96caa7866c1d55bb3309dc8307cf98c45ba
aestas <hawhawba@gmail.com>
2021-08-03 01:11:46 +0900

    파일 스토리지에 업로드 하고 포스트 생성까지 테스트 성공

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,10 @@
   const admin = require('firebase-admin')
   let inspect = require('util').inspect
   let Busboy = require('busboy')
+  let path = require('path') // path 사용
+  let os = require('os') // temp 폴더 접근 
+  let fs = require('fs') // temp 폴더에 파일을 실제로 write
+  let UUID = require('uuid-v4')
 
 /*
   config - express
@@ -21,10 +25,12 @@
   const serviceAccount = require('./serviceAccountKey.json')
     
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: 'aestagram-kwak.appspot.com'
   });
 
   const db = admin.firestore()
+  let bucket = admin.storage().bucket()
 
 /*
   endpoint - posts
@@ -50,18 +56,19 @@
     // CORS 세팅 : 모든 origin에 대해 모두 허용
     response.set('Access-Control-Allow-Origin', '*')
     
+    let uuid = UUID()
+
     var busboy = new Busboy({ headers: request.headers })
 
     let fields = {}
+    let fileData = {}
 
     busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
       console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype)
-      file.on('data', function(data) {
-        console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
-      })
-      file.on('end', function() {
-        console.log('File [' + fieldname + '] Finished')
-      })
+      // /tmp/423423-23423.png
+      let filepath = path.join(os.tmpdir(), filename)
+      file.pipe(fs.createWriteStream(filepath))
+      fileData = { filepath, mimetype }
     })
 
     busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
@@ -69,14 +76,37 @@
     })
 
     busboy.on('finish', function() {
-      db.collection('posts').doc(fields.id).set({
-        id: fields.id,
-        caption: fields.caption,
-        location: fields.location,
-        date: parseInt(fields.date),
-        imageUrl: 'https://firebasestorage.googleapis.com/v0/b/aestagram-kwak.appspot.com/o/dog2.jpg?alt=media&token=7b17d815-7aa8-4053-87f1-bd42796b9fb6'
-      })
-      response.send('Done parsing form!')
+      // 1. fileUpload
+      bucket.upload(
+        fileData.filepath,
+        {
+          uploadType: 'midea',
+          metadata: { // for the upload 
+            metadata: { // for the file 
+              contentType: fileData.mimetype,
+              firebaseStorageDownloadTokens: uuid
+            }
+          }
+        },
+        (err, uploadedFile) => {
+          if (!err) {
+            // 2. collection에 doc(데이터) 추가
+            createDocument(uploadedFile)
+          }
+        }
+      )
+      
+      function createDocument(uploadedFile) {
+        db.collection('posts').doc(fields.id).set({
+          id: fields.id,
+          caption: fields.caption,
+          location: fields.location,
+          date: parseInt(fields.date),
+          imageUrl: `https://firebasestorage.googleapis.com/v0/b/${ bucket.name }/o/${ uploadedFile.name }?alt=media&token=${ uuid }`
+        }).then(() => {
+          response.send('Post added: ' + fields.id)
+        })
+      }
     })
 
     request.pipe(busboy)
